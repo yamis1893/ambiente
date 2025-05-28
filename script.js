1ffc4e9e511e45122671dc203aee0a53d16de662
yamis1893 <mijangosyamile53@gmail.com>
2025-05-28 14:57:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+  function toggleInfo() {
+    const info = document.getElementById("info-articulo");
+    info.style.display = (info.style.display === "none" || info.style.display === "") ? "block" : "none";
+  }
+</script>
+<script>
+  function toggleInfo() {
+    const info = document.getElementById("info-articulo");
+    info.style.display = (info.style.display === "none" || info.style.display === "") ? "block" : "none";
+  }
+</script>
